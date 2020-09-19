b566432bbdf58310f226b1c04701cdd3daabd547
Felipe <fpsanger@hotmail.com>
2020-09-19 12:19:35 -0300

    Mudancas tela login e registro

--- doces-da-li-front/src/app/app-login/app-login.component.ts
+++ doces-da-li-front/src/app/app-login/app-login.component.ts
@@ -5,38 +5,35 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './app-login.component.html',
-  styleUrls: ['./app-login.component.css']
+  styleUrls: ['./app-login.component.css'],
 })
 export class AppLoginComponent implements OnInit {
-
-  form: FormGroup
+  form: FormGroup;
   submitted = false;
 
-  constructor(private formBuilder:FormBuilder, private router:Router) { 
-  }
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]],
-    })
+    });
   }
 
-  get f() { return this.form.controls; }
+  get f() {
+    return this.form.controls;
+  }
 
   onSubmit() {
     this.submitted = true;
 
     // stop here if form is invalid
     if (this.form.invalid) {
-        return;
+      return;
     }
-    this.router.navigate(['/main'])
-}
-  onRegister(){
-    this.router.navigate([
-      "/register"
-    ])
-
+    this.router.navigate(['/main']);
+  }
+  onRegister() {
+    this.router.navigate(['/register']);
   }
 }
